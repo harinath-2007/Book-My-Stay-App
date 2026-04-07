ac7a306e90ebf4b817437e4adc73cb985bd8bd52
harinath-2007 <hk7939@srmist.edu.in>
2026-04-07 14:56:11 +0530

    Refactor Room class and add inventory management
    Refactored Room class to include square feet and price per night. Updated room types and added RoomInventory and RoomSearchService classes for better room management.

--- Book My Stay App.java
+++ Book My Stay App.java
@@ -1,56 +1,78 @@
-
 abstract class Room {
-    private int beds;
-    private double price;
-    private String type;
-
-    // Constructor
-    public Room(String type, int beds, double price) {
-        this.type = type;
-        this.beds = beds;
-        this.price = price;
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
     }
-    public void displayDetails() {
-        System.out.println("Room Type : " + type);
-        System.out.println("Beds      : " + beds);
-        System.out.println("Price     : ₹" + price);
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
     }
 }
+
 class SingleRoom extends Room {
     public SingleRoom() {
-        super("Single Room", 1, 2000);
+        super(1, 250, 1500.0);
     }
 }
+
 class DoubleRoom extends Room {
     public DoubleRoom() {
-        super("Double Room", 2, 3500);
+        super(2, 400, 2500.0);
     }
 }
 class SuiteRoom extends Room {
     public SuiteRoom() {
-        super("Suite Room", 3, 6000);
+        super(3, 750, 5000.0);
     }
 }
-public class UseCase2RoomInitialization {
+class RoomInventory {
+    Room room;
+    int availableRooms;
+
+    public RoomInventory(Room room, int availableRooms) {
+        this.room = room;
+        this.availableRooms = availableRooms;
+    }
+}
+class RoomSearchService {
+
+    public static void searchRooms(RoomInventory[] inventoryList) {
+
+        System.out.println("Room Search\n");
+
+        for (RoomInventory inv : inventoryList) {
+
+            if (inv.availableRooms > 0) {
+
+                if (inv.room instanceof SingleRoom) {
+                    System.out.println("Single Room:");
+                } else if (inv.room instanceof DoubleRoom) {
+                    System.out.println("Double Room:");
+                } else if (inv.room instanceof SuiteRoom) {
+                    System.out.println("Suite Room:");
+                }
+
+                inv.room.displayRoomDetails();
+                System.out.println("Available: " + inv.availableRooms);
+                System.out.println();
+            }
+        }
+    }
+}
+public class BookMyStayApp {
     public static void main(String[] args) {
 
-        System.out.println("=====================================");
-        System.out.println("   Welcome to Book My Stay App");
-        System.out.println("   Hotel Booking System v2.1");
-        System.out.println("=====================================\n");
-        Room single = new SingleRoom();
-        Room doubleRoom = new DoubleRoom();
-        Room suite = new SuiteRoom();
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
-        int suiteAvailable = 2;
-        System.out.println("---- Room Details & Availability ----\n");
-        single.displayDetails();
-        System.out.println("Available : " + singleAvailable + "\n");
-        doubleRoom.displayDetails();
-        System.out.println("Available : " + doubleAvailable + "\n");
-        suite.displayDetails();
-        System.out.println("Available : " + suiteAvailable + "\n");
-        System.out.println("Application terminated successfully.");
+        RoomInventory[] inventory = {
+                new RoomInventory(new SingleRoom(), 5),
+                new RoomInventory(new DoubleRoom(), 3),
+                new RoomInventory(new SuiteRoom(), 2)
+        };
+
+        RoomSearchService.searchRooms(inventory);
     }
 }
